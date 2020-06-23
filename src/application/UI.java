491e9b23db9d739c42f2364c197adc1c03bc7986
paren <parentero0@gmail.com>
2020-06-22 22:59:05 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -67,5 +67,10 @@ public class UI {
 		}
 	}
 	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 	
 }
